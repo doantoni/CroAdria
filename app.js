059f18b2d1a41d11c8be75eb837e0261df182ad1
Dorian <dorian.antoniazzo@bebabit.com>
2021-07-08 17:53:22 +0200

    fixed mongo env variable

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
 
 const store = MongoStore.create({
-    mongoUrl: tempDb,
+    mongoUrl: mongoDb || tempDb,
     touchAfter: 24 * 60 * 60,
     crypto: {
         secret
